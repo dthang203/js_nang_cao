1e6032be0d0c6ad054420c5bfa17c3afc9f82421
DT Hang <dongthihang7903@gmail.com>
2023-01-30 11:30:05 +0700

    Enhanced Object Literals

---
+++ section_9/10.Enhanced Object Literals/script.js
@@ -0,0 +1,39 @@
+const weekday = ['mon','tue','wed','thu','fri','sat','sun']
+let openingHours = {
+    [weekday[3]]: {
+        open: 12,
+        close: 22
+    },
+    [weekday[4]]: {
+        open: 11,
+        close: 23
+    },
+    [weekday[5]]: {
+        open: 0,
+        close: 24
+    },
+} 
+
+let restaurant = {
+    name : 'Classico Italiano',
+    location: 'Nam tu Liem ',
+    categories : ['Italan', 'Pizzaria','Oragenic','Vegetarian'],
+    starterMenu : ['Forcaccia', 'Bruschetta','Gralic bread','Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    openingHours,
+    order (starterIndex, mainIndex){
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    } ,
+    orderDelivery({time=1,adress='no adress',mainIndex= 0,starterIndex=1}){
+        console.log(`Order received! ${this.starterMenu[starterIndex]}
+        and ${this.mainMenu[mainIndex]} will be delivered to ${adress} at ${time}`)
+    },
+
+    orderPizza(mainIngredient, ...ortherIngredient){
+        console.log(mainIngredient)
+        console.log(ortherIngredient)
+    }
+
+};
+console.log(restaurant.openingHours)
+restaurant.orderPizza('fish','tomato')
